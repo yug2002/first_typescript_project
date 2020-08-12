7376019f2b334338dce0a594c7a1be942b00876e
Yury Hamaniuk <yury_hamaniuk@epam.com>
2020-08-12 11:29:18 +0300

    fix mocha config

--- .mocharc.js
+++ .mocharc.js
@@ -8,9 +8,15 @@ module.exports = {
   timeout: SIXTY_SECS,
   spec: path.resolve('./dist/test/**/*.spec.js'),
   opts: false,
-  reporter: 'spec',
-  // reporter: 'mocha-allure-reporter',  
-  // reporterOption: [ 'targetDir=' + allureArtifactsDir ],
+  //reporter: 'spec',
+  reporter: 'mocha-allure-reporter',  
+  reporterOption: [ 'targetDir=' + allureArtifactsDir ],
+  // reporter: 'node_modules/mochawesome',
+  //   'reporter-option': [
+  //       'overwrite=true',
+  //       'reportTitle=My\ Custom\ Title',
+  //       'showPassed=true'
+  //   ],
   ui: 'bdd',
-  parallel: false
+  parallel: true
 }

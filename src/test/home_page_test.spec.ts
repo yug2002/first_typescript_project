7535898138f2faf5aa730d4c9e538349efb79f87
Yury Hamaniuk <yury_hamaniuk@epam.com>
2020-08-18 17:39:13 +0300

    fix

--- src/test/home_page_test.spec.ts
+++ src/test/home_page_test.spec.ts
@@ -17,7 +17,7 @@ describe('check home page', () => {
   describe('positive scenarios for home page', () => {
 
     beforeEach(async () => {
-      Log.debug(data.currentBrowser + ' start');
+      // Log.debug(data.currentBrowser + ' start');
       browser = Browser.getInstance();
       await browser.start();
       home = pages('Home', browser.currDriver) as HomePage;
@@ -33,7 +33,7 @@ describe('check home page', () => {
       } catch(e) {
         Log.error(e);
       } finally {
-        Log.debug(data.currentBrowser + ' stop');
+        // Log.debug(data.currentBrowser + ' stop');
         await browser.stop();
       }
     })
@@ -68,7 +68,7 @@ describe('check home page', () => {
   tests.forEach(run => {
     describe('negative scenarios for home page', () => {
       beforeEach(async () => {
-        Log.debug(data.currentBrowser + ' start');
+       // Log.debug(data.currentBrowser + ' start');
         browser = Browser.getInstance();
         await browser.start();
         home = pages('Home', browser.currDriver) as HomePage;
@@ -80,7 +80,7 @@ describe('check home page', () => {
           const title:string =  this.currentTest.title;
           await browser.takeScreenshot(`screenShot ${title}${Date.now()}.png`);
         }
-        Log.debug(data.currentBrowser + ' stop');
+       // Log.debug(data.currentBrowser + ' stop');
         await browser.stop();
       })
 
